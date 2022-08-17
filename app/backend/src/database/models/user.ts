4d31a730faa3664e297917e709e2911a12fd0122
Rafael Moreci <rafamoreci@gmail.com>
2022-08-17 18:52:24 -0300

    feat: :sparkles: Implementação do model e migration de users
    Implementado o model e migration de users + remoção de _ do seeder de users

---
+++ app/backend/src/database/models/user.ts
@@ -0,0 +1,45 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+  role: string;
+}
+
+User.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: STRING,
+      allowNull: false,
+    },
+    password: {
+      type: STRING,
+      allowNull: false,
+    },
+    email: {
+      type: STRING,
+      allowNull: false,
+    },
+    role: {
+      type: STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'users',
+    timestamps: false,
+    underscored: true,
+  },
+);
+
+export default User;
