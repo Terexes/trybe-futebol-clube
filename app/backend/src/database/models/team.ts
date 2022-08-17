adeef64ae775385c3d6cc3c120f7a38914c99061
Rafael Moreci <rafamoreci@gmail.com>
2022-08-17 19:01:45 -0300

    feat: :sparkles: Implementação das migrations e model de teams
    Implementação das migrations e model de teams + remoção de _ no seed de teams

---
+++ app/backend/src/database/models/team.ts
@@ -0,0 +1,30 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  id: number;
+  teamName: string;
+}
+
+Team.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    teamName: {
+      type: STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'teams',
+    timestamps: false,
+    underscored: true,
+  },
+);
+
+export default Team;
